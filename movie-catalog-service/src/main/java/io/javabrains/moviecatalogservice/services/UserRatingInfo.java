36f04df04ce0a2769f4e55a3644b934b7864846d
Maxim Savenko <maxim.savenko@gmail.com>
2020-11-21 09:15:15 +0300

    maxix

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/services/UserRatingInfo.java
@@ -0,0 +1,31 @@
+package io.javabrains.moviecatalogservice.services;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import io.javabrains.moviecatalogservice.models.Rating;
+import io.javabrains.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Service
+public class UserRatingInfo {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    public UserRating getUserRating(String userId) {
+        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+        return userRating;
+    }
+
+    private UserRating getFallbackUserRating(String userId) {
+        UserRating userRating = new UserRating();
+        userRating.setUserId(userId);
+        userRating.setRatings(Arrays.asList(
+                new Rating("0", 0)
+        ));
+        return userRating;
+    }
+}
